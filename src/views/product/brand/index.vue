e294a2305ec259ce8bb7262a060d137780d6f21c
hzy <hzy381798094@163.com>
2024-01-20 11:30:55 +0800

    实现品牌管理的添加新品牌以及修改现有品牌数据功能，并添加了表单验证模块完善了细节

--- src/views/product/brand/index.vue
+++ src/views/product/brand/index.vue
@@ -5,7 +5,7 @@
       type="primary"
       size="default"
       :icon="Plus"
-      @click="openChangeOrCreateBrand('添加品牌')"
+      @click="openChangeOrCreateBrand({})"
     >
       添加品牌
     </el-button>
@@ -26,7 +26,7 @@
             type="warning"
             :icon="Edit"
             size="small"
-            @click="openChangeOrCreateBrand('修改品牌')"
+            @click="openChangeOrCreateBrand(row)"
           />
           <el-button type="danger" :icon="Delete" size="small" />
         </template>
@@ -48,11 +48,16 @@
   <!-- 添加 && 修改品牌数据时弹出该对话框 -->
   <el-dialog v-model="dialogVisible" :title="dialogTitle">
     <!-- 中间内容区域 -->
-    <el-form style="width: 80%">
-      <el-form-item label="品牌名称" label-width="90px">
+    <el-form
+      style="width: 80%"
+      ref="brandFormRef"
+      :model="brandParams"
+      :rules="rules"
+    >
+      <el-form-item label="品牌名称" prop="tmName" label-width="100px">
         <el-input v-model="brandParams.tmName" placeholder="请输入品牌名称" />
       </el-form-item>
-      <el-form-item label="品牌 LOGO" label-width="90px">
+      <el-form-item label="品牌 LOGO" prop="logoUrl" label-width="100px">
         <el-upload
           class="avatar-uploader"
           action="/api/admin/product/fileUpload"
@@ -83,9 +88,12 @@
   /** EL 组件引入 */
   import { Delete, Edit, Plus } from '@element-plus/icons-vue'
   import { ElMessage } from 'element-plus'
-  import type { UploadProps } from 'element-plus'
+  import type { FormInstance, FormRules, UploadProps } from 'element-plus'
   /** 接口引入 */
-  import { requestGetHasBrandByPageAPI } from '@/api/product/brand'
+  import {
+    requestGetHasBrandByPageAPI,
+    requestAddOrUpdateBrandAPI,
+  } from '@/api/product/brand'
   /** 类型引入 */
   import {
     TRecordsItem,
@@ -93,12 +101,37 @@
     TAddOrUpdateBrandParams,
   } from '@/api/product/brand/type'
   /** API 引入 */
-  import { ref, onMounted, reactive } from 'vue'
+  import { ref, onMounted, reactive, nextTick } from 'vue'
 
   /** 组件挂载后调用 */
   onMounted(() => {
     // 获得已有的品牌数据
     getHasBrandData()
+  })
+
+  /** 表单校验 数据 && 方法 */
+  // 获取表单实例
+  const brandFormRef = ref<FormInstance>()
+  // 品牌名的自定义校验规则
+  const tmNameValidator = (rule: any, value: any, callback: any) => {
+    if (value.trim().length >= 2) {
+      callback()
+    } else {
+      callback(new Error('品牌名过短'))
+    }
+  }
+  // 上传图片的自定义校验规则
+  const logoUrlValidator = (rule: any, value: any, callback: any) => {
+    if (value) {
+      callback()
+    } else {
+      callback(new Error('请上传品牌图片！'))
+    }
+  }
+  // 表单规则校验对象
+  const rules = reactive<FormRules<typeof brandParams>>({
+    tmName: [{ required: true, trigger: 'blur', validator: tmNameValidator }],
+    logoUrl: [{ required: true, validator: logoUrlValidator }],
   })
 
   /** 添加 && 修改品牌数据对话框的数据 && 方法 */
@@ -112,14 +145,55 @@
   // 控制对话框的标题内容
   let dialogTitle = ref<string>('')
   // 添加以及编辑现有品牌的按钮点击事件
-  const openChangeOrCreateBrand = (title: string) => {
-    dialogTitle.value = title
+  const openChangeOrCreateBrand = (item: TRecordsItem | any) => {
+    // 清空曾收集的数据
+    brandParams.id = undefined
+    brandParams.tmName = ''
+    brandParams.logoUrl = ''
+    // 打开对话框
     dialogVisible.value = true
+    if (Object.keys(item).length) {
+      // 设置标题名
+      dialogTitle.value = '修改界面'
+      // 编辑修改需要设置初始值
+      brandParams.id = item.id
+      brandParams.tmName = item.tmName
+      brandParams.logoUrl = item.logoUrl
+    } else {
+      // 设置标题名
+      dialogTitle.value = '添加界面'
+    }
+    // 当 form 实例挂载后，清除掉上一次的校验结果【红色提示文字】
+    nextTick(() => {
+      brandFormRef.value?.clearValidate('tmName')
+      brandFormRef.value?.clearValidate('logoUrl')
+    })
   }
   // 对话框确定按钮的点击事件
-  const dialogConfirm = () => {
-    // 先设置为隐藏对话框
-    dialogVisible.value = false
+  const dialogConfirm = async () => {
+    try {
+      // first: 进行一次表单验证
+      await brandFormRef.value?.validate()
+      // 调用请求接口方法：该方法返回的是一个 Promise
+      await addOrUpdateBrandData(brandParams)
+      // 先设置为隐藏对话框
+      dialogVisible.value = false
+      // 弹出对话框，告诉用户添加商品成功
+      // 根据是否带有 id 值区分提示内容
+      ElMessage({
+        type: 'success',
+        message: brandParams.id ? '已修改当前品牌' : '成功添加新品牌',
+      })
+      // 获取已有品牌数据
+      getHasBrandData()
+    } catch (error) {
+      ElMessage({
+        type: 'error',
+        message: brandParams.id
+          ? '修改品牌时出错，请稍后再试'
+          : '当前添加品牌失败，请重新尝试',
+      })
+    }
   }
   // 上传组件：-> 图片上传之前的钩子
   // 方法目的：用于对文件进行过筛，筛选掉非图片类型以及 > 4M 的文件
@@ -149,7 +223,10 @@
   // 上传组件： -> 图片上传成功后的钩子
   // 方法目的： 将返回的上传成功的图片路径保存到变量中
   const handleBrandImgSuccess: UploadProps['onSuccess'] = (response) => {
+    // 将返回数据保存到收集对象中
     brandParams.logoUrl = response.data
+    // 并且清除图片验证的红色提示文字
+    brandFormRef.value?.clearValidate('logoUrl')
   }
 
   /** 分页器组件数据&&方法 */
@@ -184,6 +261,18 @@
       // 存储数据
       brandList.value = result.data.records
       totalData.value = result.data.total
+    }
+  }
+
+  // 请求添加&&修改品牌数据
+  const addOrUpdateBrandData = async (data: TAddOrUpdateBrandParams) => {
+    const result = await requestAddOrUpdateBrandAPI(data)
+    if (result.code === 200) {
+      // 添加成功
+      return result.message
+    } else {
+      // 添加失败
+      return Promise.reject(new Error(result.message))
     }
   }
 </script>
