3d41455517f9eddb8ed694e51186964a598491e4
hzy <hzy381798094@163.com>
2024-02-05 16:24:36 +0800

    添加用户数据，更新用户数据接口部署完成，TS类型已补齐。添加功能完成，更新|修改功能待测试

--- src/api/acl/user/index.ts
+++ src/api/acl/user/index.ts
@@ -2,14 +2,27 @@
 import request from '@/utils/request'
 
 /** 类型引入 */
-import type { UserListResponseData } from './type'
+import type { UserListResponseData, UserListItem, UserManageResponseData } from './type'
 
 /** 接口地址枚举 */
 enum API {
   // 获取管理用户分页列表
   GET_USER_LIST_BY_PAGE__URL = '/admin/acl/user/',
+  // 新增管理用户
+  POST_ADD_USER__URL = '/admin/acl/user/save',
+  // 修改管理用户
+  PUT_UPDATE_USER__URL = '/admin/acl/user/update',
 }
 
 // 获取管理用户分页列表【根据分页】
 export const requestUserListByPageAPI = (page: number, limit: number) =>
   request.get<any, UserListResponseData>(API.GET_USER_LIST_BY_PAGE__URL + `${page}/${limit}`)
+
+// 添加 or 修改用户信息
+export const requestAddOrUpdateUserAPI = (data: UserListItem) => {
+  if (data.id) {
+    return request.put<any, UserManageResponseData<string | null>>(API.PUT_UPDATE_USER__URL, data)
+  } else {
+    return request.post<any, UserManageResponseData<string | null>>(API.POST_ADD_USER__URL, data)
+  }
+}
--- src/views/admin/userAdmin/index.vue
+++ src/views/admin/userAdmin/index.vue
@@ -1,81 +1,110 @@
 <template>
-  <!-- form 卡片 -->
-  <el-card class="form-card">
-    <el-form class="form-box" inline>
-      <el-form-item label="用户名：">
-        <el-input placeholder="请输入用户名" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">搜索</el-button>
-        <el-button>重置</el-button>
-      </el-form-item>
-    </el-form>
-  </el-card>
-  <!-- 列表展示 卡片 -->
-  <el-card class="show-card">
-    <el-button type="primary">添加</el-button>
-    <el-button type="danger">批量删除</el-button>
-    <!-- 表格 -->
-    <el-table :data="userList" class="table-box" border>
-      <el-table-column type="selection" width="70" align="center" />
-      <el-table-column label="#" type="index" width="80" align="center" />
-      <el-table-column label="id" prop="id" width="80" align="center" />
-      <el-table-column
-        label="用户名字"
-        prop="name"
-        width="110"
-        align="center"
-        show-overflow-tooltip
+  <div>
+    <!-- form 卡片 -->
+    <el-card class="form-card">
+      <el-form class="form-box" inline>
+        <el-form-item label="用户名：">
+          <el-input placeholder="请输入用户名" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">搜索</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 列表展示 卡片 -->
+    <el-card class="show-card">
+      <el-button @click="handleAddUserData" type="primary">添加</el-button>
+      <el-button type="danger">批量删除</el-button>
+      <!-- 表格 -->
+      <el-table :data="userList" class="table-box" border>
+        <el-table-column type="selection" width="70" align="center" />
+        <el-table-column label="#" type="index" width="80" align="center" />
+        <el-table-column label="id" prop="id" width="80" align="center" />
+        <el-table-column
+          label="用户名字"
+          prop="name"
+          width="110"
+          align="center"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          label="用户名称"
+          prop="username"
+          width="110"
+          align="center"
+          show-overflow-tooltip
+        />
+        <el-table-column label="用户角色" prop="roleName" show-overflow-tooltip />
+        <el-table-column
+          label="创建时间"
+          prop="createTime"
+          width="110"
+          align="center"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          label="更新时间"
+          prop="updateTime"
+          width="110"
+          align="center"
+          show-overflow-tooltip
+        />
+        <el-table-column label="操作" width="270" align="center">
+          <template #default>
+            <el-button :icon="User" type="primary" size="small">分配角色</el-button>
+            <el-button :icon="Edit" type="warning" size="small">编辑</el-button>
+            <el-button :icon="Delete" type="danger" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页器 -->
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        background
+        layout="prev, pager, next, jumper,->,sizes, total"
+        :total="totalData"
+        @current-change="fetchUserList"
+        @size-change="handleSizeChange"
       />
-      <el-table-column
-        label="用户名称"
-        prop="username"
-        width="110"
-        align="center"
-        show-overflow-tooltip
-      />
-      <el-table-column label="用户角色" prop="roleName" show-overflow-tooltip />
-      <el-table-column
-        label="创建时间"
-        prop="createTime"
-        width="110"
-        align="center"
-        show-overflow-tooltip
-      />
-      <el-table-column
-        label="更新时间"
-        prop="updateTime"
-        width="110"
-        align="center"
-        show-overflow-tooltip
-      />
-      <el-table-column label="操作" width="270" align="center">
-        <template #default>
-          <el-button :icon="User" type="primary" size="small">分配角色</el-button>
-          <el-button :icon="Edit" type="warning" size="small">编辑</el-button>
-          <el-button :icon="Delete" type="danger" size="small">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页器 -->
-    <el-pagination
-      v-model:current-page="pageNo"
-      v-model:page-size="pageSize"
-      :page-sizes="[3, 5, 7, 9]"
-      background
-      layout="prev, pager, next, jumper,->,sizes, total"
-      :total="totalData"
-      @current-change="fetchUserList"
-      @size-change="handleSizeChange"
-    />
-  </el-card>
+    </el-card>
+    <!-- 新增用户抽屉 -->
+    <el-drawer v-model="userFormDrawer">
+      <template #header>
+        <h4>添加用户</h4>
+      </template>
+      <template #default>
+        <el-form>
+          <el-form-item label="用户姓名">
+            <el-input v-model="userInfoParams.username" placeholder="请您输入用户姓名" />
+          </el-form-item>
+          <el-form-item label="用户昵称">
+            <el-input v-model="userInfoParams.name" placeholder="请您输入用户昵称" />
+          </el-form-item>
+          <el-form-item label="用户密码">
+            <el-input
+              v-model="userInfoParams.password"
+              placeholder="请您输入用户密码"
+              type="password"
+            />
+          </el-form-item>
+        </el-form>
+      </template>
+      <template #footer>
+        <el-button @click="drawerCancel">取消</el-button>
+        <el-button @click="drawerConfirm" type="primary">确定</el-button>
+      </template>
+    </el-drawer>
+  </div>
 </template>
 
 <script setup lang="ts">
   /** API */
-  import { ref, onMounted } from 'vue'
+  import { ref, reactive, onMounted } from 'vue'
   /** 接口引入 */
-  import { requestUserListByPageAPI } from '@/api/acl/user'
+  import { requestUserListByPageAPI, requestAddOrUpdateUserAPI } from '@/api/acl/user'
   /** 接口类型引入 */
   import type { UserListItem } from '@/api/acl/user/type'
   /** EL 组件引入 */
@@ -93,6 +122,11 @@
 
   /** 用户管理数据存储相关 */
   const userList = ref<UserListItem[]>([])
+  const userInfoParams = reactive<UserListItem>({
+    name: '',
+    password: '',
+    username: '',
+  })
 
   /** 分页器相关 */
   const pageNo = ref<number>(1)
@@ -100,6 +134,38 @@
   const totalData = ref<number>(20)
   const handleSizeChange = () => {
     fetchUserList()
+  }
+
+  /** 抽屉相关 */
+  const userFormDrawer = ref<boolean>(false)
+  const drawerCancel = () => {
+    userFormDrawer.value = false
+  }
+  const drawerConfirm = async () => {
+    try {
+      // 发送请求
+      await addOrUpdateUserInfo(userInfoParams)
+      ElMessage.success(userInfoParams.id ? '修改成功' : '成功创建')
+      // 关掉抽屉
+      userFormDrawer.value = false
+      // 重新加载数据
+      await fetchUserList()
+    } catch (e) {
+      ElMessage.error('操作失败，请检查数据是否符合标准')
+    }
+  }
+
+  /** 【表格外】操作表格数据相关 */
+  // 添加用户按钮 @click
+  const handleAddUserData = () => {
+    // 清空参数
+    Object.assign(userInfoParams, {
+      name: '',
+      password: '',
+      username: '',
+    })
+    // 打开抽屉
+    userFormDrawer.value = true
   }
 
   /** 数据请求相关 */
@@ -117,6 +183,15 @@
       return data
     } else {
       return Promise.reject(new Error('数据获取失败'))
+    }
+  }
+  const addOrUpdateUserInfo = async (data: UserListItem) => {
+    const result = await requestAddOrUpdateUserAPI(data)
+    const { code } = result
+    if (code === 200) {
+      return 'ok'
+    } else {
+      return Promise.reject(new Error('失败'))
     }
   }
 </script>
