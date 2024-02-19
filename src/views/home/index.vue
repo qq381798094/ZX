77b2a741b5d9141784bef805eaa5a5eefb9d7ab6
hzy <hzy381798094@163.com>
2024-02-19 15:50:00 +0800

    导入部分需要的SVG文件，完成首页的页面展示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <h1>home</h1>
+    <el-card>
+      <div class="box">
+        <img class="avatar-img" :src="userStore.avatar" alt="" />
+        <div class="user-info">
+          <span class="user-name">{{ getTime() + '好，' + userStore.username }}</span>
+          <span class="user-location">甄选运营平台</span>
+        </div>
+      </div>
+    </el-card>
+    <!-- SVG 图片 -->
+    <div class="welcome-box">
+      <svg-icon name="welcome" width="500px" height="300px" />
+    </div>
   </div>
 </template>
 
@@ -11,6 +23,8 @@
   import useUserStore from '@/store/modules/user'
   /** EL 组件 */
   import { ElNotification } from 'element-plus'
+  /** 工具引入 */
+  import { getTime } from '@/utils/time'
 
   /** 实例化仓库 */
   // 用户仓库
@@ -29,5 +43,40 @@
     }
   })
 </script>
+<script lang="ts">
+  export default {
+    name: 'Home',
+  }
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .box {
+    display: flex;
+  }
+  .avatar-img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .user-info {
+    margin: 15px 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .user-name {
+      font-size: 22px;
+      font-weight: bold;
+    }
+
+    .user-location {
+      font-size: 15px;
+      font-style: italic;
+    }
+  }
+  .welcome-box {
+    margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
